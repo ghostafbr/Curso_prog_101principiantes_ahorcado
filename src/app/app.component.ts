60a48a16135a3b8f2c7af2b306c3bce046200a0e
Andres Bolaños <ghostafbr@gmail.com>
2019-07-28 20:16:34 -0500

    Listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'ahorcado';
+
+  palabra = 'PANDERITOSPIONONITOS';
+  palabraOculta = '';
+  intentos = 0;
+  gano = false;
+  perdio = false;
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor() {
+
+    this.palabraOculta = '_ '.repeat( this.palabra.length );
+
+  }
+
+  comprobar(letra ) {
+
+    this.existeLetra(letra);
+
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for (let i = 0; i < this.palabra.length; i++) {
+
+      if ( this.palabra[i] === letra) {
+      palabraOcultaArr[i] = letra;
+      }
+
+    }
+
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
+  }
+
+
+verificaGane() {
+
+  const palabraArr = this.palabraOculta.split(' ');
+  const palabraEvaluar = palabraArr.join('');
+  console.log(palabraEvaluar);
+
+  if (palabraEvaluar === this.palabra) {
+
+    this.gano = true;
+    console.log('Has Ganado!');
+  }
+
+  if ( this.intentos >= 9) {
+    this.perdio = true;
+    console.log('Has perdido');
+  }
+
+}
+
+
+  existeLetra( letra ) {
+
+    if (this.palabra.indexOf(letra) >= 0) {
+        // console.log('La letra existe ' + letra);
+      } else {
+      // console.log('La letra no existe ' + letra);
+      this.intentos ++;
+    }
+
+  }
+
 }
